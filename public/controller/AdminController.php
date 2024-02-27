da9a6d089a61efbab8c1f2b46af3593db3333fbc
Fremensito <alexeykr98@gmail.com>
2024-02-27 13:50:56 +0100

    panel de administrador

--- public/controller/AdminController.php
+++ public/controller/AdminController.php
@@ -1,2 +1,21 @@
 <?php
-    echo "hola";
+    include_once "../models/Event.php";
+    include_once "../models/User.php";
+    include_once "../models/Post.php";
+
+    session_start();
+
+    if(isset($_SESSION["user"]) && $_SESSION["user"]->nick == "admin"){
+        $raw = Event::GetAll();
+        $eventos = array_filter($raw, fn($event) => !$event->state);
+
+        $raw = Post::GetAll();
+
+        //No tiene ->state;
+        $posts = $raw;
+
+        include_once "../views/admin_panel.php";
+    }
+    else{
+        header("Location: LoginController.php");
+    }
